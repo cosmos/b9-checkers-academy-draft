58912bdbf5db7610c8eb64154150c58126583d2b
Xavier Leprêtre <xavier.lepretre@gmail.com>
2021-09-27 18:16:17 +0400

    Fixed omission about MsgCreateGame.

--- x/checkers/client/cli/tx_create_game.go
+++ x/checkers/client/cli/tx_create_game.go
@@ -14,19 +14,23 @@ var _ = strconv.Itoa(0)
 
 func CmdCreateGame() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "create-game [red] [black]",
+		Use:   "create-game [red] [black] [wager]",
 		Short: "Broadcast message createGame",
-		Args:  cobra.ExactArgs(2),
+		Args:  cobra.ExactArgs(3),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			argsRed := string(args[0])
 			argsBlack := string(args[1])
+			argsWager, err := strconv.ParseUint(args[2], 10, 64)
+			if err != nil {
+				return err
+			}
 
 			clientCtx, err := client.GetClientTxContext(cmd)
 			if err != nil {
 				return err
 			}
 
-			msg := types.NewMsgCreateGame(clientCtx.GetFromAddress().String(), string(argsRed), string(argsBlack))
+			msg := types.NewMsgCreateGame(clientCtx.GetFromAddress().String(), string(argsRed), string(argsBlack), uint64(argsWager))
 			if err := msg.ValidateBasic(); err != nil {
 				return err
 			}
