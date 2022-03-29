37047b20878b712300f7ee1fbef79d9470777cfb
Xavier Leprêtre <xavier.lepretre@gmail.com>
2022-03-29 16:42:01 +0400

    Added further tests of msg server of create game.

--- x/checkers/keeper/msg_server_create_game_test.go
+++ x/checkers/keeper/msg_server_create_game_test.go
@@ -35,3 +35,207 @@ func TestCreateGame(t *testing.T) {
 		IdValue: "1",
 	}, *createResponse)
 }
+
+func TestCreate1GameHasSaved(t *testing.T) {
+	msgSrvr, keeper, context := setupMsgServerCreateGame(t)
+	msgSrvr.CreateGame(context, &types.MsgCreateGame{
+		Creator: alice,
+		Red:     bob,
+		Black:   carol,
+	})
+	nextGame, found := keeper.GetNextGame(sdk.UnwrapSDKContext(context))
+	require.True(t, found)
+	require.EqualValues(t, types.NextGame{
+		Creator: "",
+		IdValue: 2,
+	}, nextGame)
+	game1, found1 := keeper.GetStoredGame(sdk.UnwrapSDKContext(context), "1")
+	require.True(t, found1)
+	require.EqualValues(t, types.StoredGame{
+		Creator: alice,
+		Index:   "1",
+		Game:    "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:    "b",
+		Red:     bob,
+		Black:   carol,
+	}, game1)
+}
+
+func TestCreate1GameGetAll(t *testing.T) {
+	msgSrvr, keeper, context := setupMsgServerCreateGame(t)
+	msgSrvr.CreateGame(context, &types.MsgCreateGame{
+		Creator: alice,
+		Red:     bob,
+		Black:   carol,
+	})
+	games := keeper.GetAllStoredGame(sdk.UnwrapSDKContext(context))
+	require.Len(t, games, 1)
+	require.EqualValues(t, types.StoredGame{
+		Creator: alice,
+		Index:   "1",
+		Game:    "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:    "b",
+		Red:     bob,
+		Black:   carol,
+	}, games[0])
+}
+
+func TestCreate3Games(t *testing.T) {
+	msgSrvr, _, context := setupMsgServerCreateGame(t)
+	msgSrvr.CreateGame(context, &types.MsgCreateGame{
+		Creator: alice,
+		Red:     bob,
+		Black:   carol,
+	})
+	createResponse2, err2 := msgSrvr.CreateGame(context, &types.MsgCreateGame{
+		Creator: bob,
+		Red:     carol,
+		Black:   alice,
+	})
+	require.Nil(t, err2)
+	require.EqualValues(t, types.MsgCreateGameResponse{
+		IdValue: "2",
+	}, *createResponse2)
+	createResponse3, err3 := msgSrvr.CreateGame(context, &types.MsgCreateGame{
+		Creator: carol,
+		Red:     alice,
+		Black:   bob,
+	})
+	require.Nil(t, err3)
+	require.EqualValues(t, types.MsgCreateGameResponse{
+		IdValue: "3",
+	}, *createResponse3)
+}
+
+func TestCreate3GamesHasSaved(t *testing.T) {
+	msgSrvr, keeper, context := setupMsgServerCreateGame(t)
+	msgSrvr.CreateGame(context, &types.MsgCreateGame{
+		Creator: alice,
+		Red:     bob,
+		Black:   carol,
+	})
+	msgSrvr.CreateGame(context, &types.MsgCreateGame{
+		Creator: bob,
+		Red:     carol,
+		Black:   alice,
+	})
+	msgSrvr.CreateGame(context, &types.MsgCreateGame{
+		Creator: carol,
+		Red:     alice,
+		Black:   bob,
+	})
+	nextGame, found := keeper.GetNextGame(sdk.UnwrapSDKContext(context))
+	require.True(t, found)
+	require.EqualValues(t, types.NextGame{
+		Creator: "",
+		IdValue: 4,
+	}, nextGame)
+	game1, found1 := keeper.GetStoredGame(sdk.UnwrapSDKContext(context), "1")
+	require.True(t, found1)
+	require.EqualValues(t, types.StoredGame{
+		Creator: alice,
+		Index:   "1",
+		Game:    "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:    "b",
+		Red:     bob,
+		Black:   carol,
+	}, game1)
+	game2, found2 := keeper.GetStoredGame(sdk.UnwrapSDKContext(context), "2")
+	require.True(t, found2)
+	require.EqualValues(t, types.StoredGame{
+		Creator: bob,
+		Index:   "2",
+		Game:    "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:    "b",
+		Red:     carol,
+		Black:   alice,
+	}, game2)
+	game3, found3 := keeper.GetStoredGame(sdk.UnwrapSDKContext(context), "3")
+	require.True(t, found3)
+	require.EqualValues(t, types.StoredGame{
+		Creator: carol,
+		Index:   "3",
+		Game:    "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:    "b",
+		Red:     alice,
+		Black:   bob,
+	}, game3)
+}
+
+func TestCreate3GamesGetAll(t *testing.T) {
+	msgSrvr, keeper, context := setupMsgServerCreateGame(t)
+	msgSrvr.CreateGame(context, &types.MsgCreateGame{
+		Creator: alice,
+		Red:     bob,
+		Black:   carol,
+	})
+	msgSrvr.CreateGame(context, &types.MsgCreateGame{
+		Creator: bob,
+		Red:     carol,
+		Black:   alice,
+	})
+	msgSrvr.CreateGame(context, &types.MsgCreateGame{
+		Creator: carol,
+		Red:     alice,
+		Black:   bob,
+	})
+	games := keeper.GetAllStoredGame(sdk.UnwrapSDKContext(context))
+	require.Len(t, games, 3)
+	require.EqualValues(t, types.StoredGame{
+		Creator: alice,
+		Index:   "1",
+		Game:    "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:    "b",
+		Red:     bob,
+		Black:   carol,
+	}, games[0])
+	require.EqualValues(t, types.StoredGame{
+		Creator: bob,
+		Index:   "2",
+		Game:    "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:    "b",
+		Red:     carol,
+		Black:   alice,
+	}, games[1])
+	require.EqualValues(t, types.StoredGame{
+		Creator: carol,
+		Index:   "3",
+		Game:    "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:    "b",
+		Red:     alice,
+		Black:   bob,
+	}, games[2])
+}
+
+func TestCreateGameFarFuture(t *testing.T) {
+	msgSrvr, keeper, context := setupMsgServerCreateGame(t)
+	keeper.SetNextGame(sdk.UnwrapSDKContext(context), types.NextGame{
+		Creator: "",
+		IdValue: 1024,
+	})
+	createResponse, err := msgSrvr.CreateGame(context, &types.MsgCreateGame{
+		Creator: alice,
+		Red:     bob,
+		Black:   carol,
+	})
+	require.Nil(t, err)
+	require.EqualValues(t, types.MsgCreateGameResponse{
+		IdValue: "1024",
+	}, *createResponse)
+	nextGame, found := keeper.GetNextGame(sdk.UnwrapSDKContext(context))
+	require.True(t, found)
+	require.EqualValues(t, types.NextGame{
+		Creator: "",
+		IdValue: 1025,
+	}, nextGame)
+	game1, found1 := keeper.GetStoredGame(sdk.UnwrapSDKContext(context), "1024")
+	require.True(t, found1)
+	require.EqualValues(t, types.StoredGame{
+		Creator: alice,
+		Index:   "1024",
+		Game:    "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:    "b",
+		Red:     bob,
+		Black:   carol,
+	}, game1)
+}
