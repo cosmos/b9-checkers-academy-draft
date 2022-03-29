d35fe84c8e42b3ec67d560d244fda04b4d9827b4
Xavier Leprêtre <xavier.lepretre@gmail.com>
2022-03-29 18:30:44 +0400

    Added dummy test for play move.

---
+++ x/checkers/keeper/msg_server_play_move_test.go
@@ -0,0 +1,39 @@
+package keeper_test
+
+import (
+	"context"
+	"testing"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+	"github.com/xavierlepretre/checkers/x/checkers"
+	"github.com/xavierlepretre/checkers/x/checkers/keeper"
+	"github.com/xavierlepretre/checkers/x/checkers/types"
+)
+
+func setupMsgServerPlayMove(t testing.TB) (types.MsgServer, keeper.Keeper, context.Context) {
+	k, ctx := setupKeeper(t)
+	checkers.InitGenesis(ctx, *k, *types.DefaultGenesis())
+	return keeper.NewMsgServerImpl(*k), *k, sdk.WrapSDKContext(ctx)
+}
+
+func TestPlayMove(t *testing.T) {
+	msgServer, _, context := setupMsgServerCreateGame(t)
+	msgServer.CreateGame(context, &types.MsgCreateGame{
+		Creator: alice,
+		Red:     bob,
+		Black:   carol,
+	})
+	playMoveResponse, err := msgServer.PlayMove(context, &types.MsgPlayMove{
+		Creator: carol,
+		IdValue: "1",
+		FromX:   1,
+		FromY:   2,
+		ToX:     2,
+		ToY:     3,
+	})
+	require.Nil(t, err)
+	require.EqualValues(t, types.MsgPlayMoveResponse{
+		IdValue: "",
+	}, *playMoveResponse)
+}
