792d879736023e3276f615ae99dbdd1c6d267ac0
Xavier Leprêtre <xavier.lepretre@gmail.com>
2022-03-31 00:24:59 +0400

    Added unit tests for events emitted on create and play.

--- x/checkers/keeper/msg_server_create_game_test.go
+++ x/checkers/keeper/msg_server_create_game_test.go
@@ -80,6 +80,31 @@ func TestCreate1GameGetAll(t *testing.T) {
 	}, games[0])
 }
 
+func TestCreate1GameEmitted(t *testing.T) {
+	msgSrvr, _, context := setupMsgServerCreateGame(t)
+	msgSrvr.CreateGame(context, &types.MsgCreateGame{
+		Creator: alice,
+		Red:     bob,
+		Black:   carol,
+	})
+	ctx := sdk.UnwrapSDKContext(context)
+	require.NotNil(t, ctx)
+	events := sdk.StringifyEvents(ctx.EventManager().ABCIEvents())
+	require.Len(t, events, 1)
+	event := events[0]
+	require.EqualValues(t, sdk.StringEvent{
+		Type: "message",
+		Attributes: []sdk.Attribute{
+			{Key: "module", Value: "checkers"},
+			{Key: "action", Value: "NewGameCreated"},
+			{Key: "Creator", Value: alice},
+			{Key: "Index", Value: "1"},
+			{Key: "Red", Value: bob},
+			{Key: "Black", Value: carol},
+		},
+	}, event)
+}
+
 func TestCreateGameRedAddressBad(t *testing.T) {
 	msgServer, _, context := setupMsgServerCreateGame(t)
 	createResponse, err := msgServer.CreateGame(context, &types.MsgCreateGame{
--- x/checkers/keeper/msg_server_play_move_test.go
+++ x/checkers/keeper/msg_server_play_move_test.go
@@ -100,6 +100,33 @@ func TestPlayMoveWrongPieceAtDestination(t *testing.T) {
 	require.Equal(t, "Already piece at destination position: {0 1}: wrong move", err.Error())
 }
 
+func TestPlayMoveEmitted(t *testing.T) {
+	msgServer, _, context := setupMsgServerWithOneGameForPlayMove(t)
+	msgServer.PlayMove(context, &types.MsgPlayMove{
+		Creator: carol,
+		IdValue: "1",
+		FromX:   1,
+		FromY:   2,
+		ToX:     2,
+		ToY:     3,
+	})
+	ctx := sdk.UnwrapSDKContext(context)
+	require.NotNil(t, ctx)
+	events := sdk.StringifyEvents(ctx.EventManager().ABCIEvents())
+	require.Len(t, events, 1)
+	event := events[0]
+	require.Equal(t, event.Type, "message")
+	require.EqualValues(t, []sdk.Attribute{
+		{Key: "module", Value: "checkers"},
+		{Key: "action", Value: "MovePlayed"},
+		{Key: "Creator", Value: carol},
+		{Key: "IdValue", Value: "1"},
+		{Key: "CapturedX", Value: "-1"},
+		{Key: "CapturedY", Value: "-1"},
+		{Key: "Winner", Value: "NO_PLAYER"},
+	}, event.Attributes[6:])
+}
+
 func TestPlayMove2(t *testing.T) {
 	msgServer, _, context := setupMsgServerWithOneGameForPlayMove(t)
 	msgServer.PlayMove(context, &types.MsgPlayMove{
