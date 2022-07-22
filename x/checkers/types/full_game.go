7916a46d7d28e3d57474eda5172793422d676123
Xavier Lepretre <xavier.lepretre@gmail.com>
2022-07-23 00:30:05 +0400

    Add stored game helper functions.

---
+++ x/checkers/types/full_game.go
@@ -0,0 +1,45 @@
+package types
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/b9lab/checkers/x/checkers/rules"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+)
+
+func (storedGame StoredGame) GetBlackAddress() (black sdk.AccAddress, err error) {
+	black, errBlack := sdk.AccAddressFromBech32(storedGame.Black)
+	return black, sdkerrors.Wrapf(errBlack, ErrInvalidBlack.Error(), storedGame.Black)
+}
+
+func (storedGame StoredGame) GetRedAddress() (red sdk.AccAddress, err error) {
+	red, errRed := sdk.AccAddressFromBech32(storedGame.Red)
+	return red, sdkerrors.Wrapf(errRed, ErrInvalidRed.Error(), storedGame.Red)
+}
+
+func (storedGame StoredGame) ParseGame() (game *rules.Game, err error) {
+	board, errBoard := rules.Parse(storedGame.Board)
+	if errBoard != nil {
+		return nil, sdkerrors.Wrapf(errBoard, ErrGameNotParseable.Error())
+	}
+	board.Turn = rules.StringPieces[storedGame.Turn].Player
+	if board.Turn.Color == "" {
+		return nil, sdkerrors.Wrapf(errors.New(fmt.Sprintf("Turn: %s", storedGame.Turn)), ErrGameNotParseable.Error())
+	}
+	return board, nil
+}
+
+func (storedGame StoredGame) Validate() (err error) {
+	_, err = storedGame.GetBlackAddress()
+	if err != nil {
+		return err
+	}
+	_, err = storedGame.GetRedAddress()
+	if err != nil {
+		return err
+	}
+	_, err = storedGame.ParseGame()
+	return err
+}
