c06cf79eded98b5be4661ad29e13bef06c926b57
Xavier Lepretre <xavier.lepretre@gmail.com>
2022-07-23 00:30:05 +0400

    Add stored game helper functions.

---
+++ x/checkers/testutil/constants.go
@@ -0,0 +1,7 @@
+package testutil
+
+const (
+	Alice = "cosmos1jmjfq0tplp9tmx4v9uemw72y4d2wa5nr3xn9d3"
+	Bob   = "cosmos1xyxs3skf3f4jfqeuv89yyaqvjc6lffavxqhc8g"
+	Carol = "cosmos1e0w5t53nrq7p66fye6c8p0ynyhf6y24l4yuxd7"
+)
