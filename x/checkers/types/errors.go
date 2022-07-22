37d44d1270283ea58487a74682866cc9252325d0
Xavier Lepretre <xavier.lepretre@gmail.com>
2022-07-23 00:30:05 +0400

    Add stored game helper functions.
    Signed-off-by: Pranay Valson <pranay.valson@gmail.com>

--- x/checkers/types/errors.go
+++ x/checkers/types/errors.go
@@ -8,5 +8,7 @@ import (
 
 // x/checkers module sentinel errors
 var (
-	ErrSample = sdkerrors.Register(ModuleName, 1100, "sample error")
+	ErrInvalidBlack     = sdkerrors.Register(ModuleName, 1100, "black address is invalid: %s")
+	ErrInvalidRed       = sdkerrors.Register(ModuleName, 1101, "red address is invalid: %s")
+	ErrGameNotParseable = sdkerrors.Register(ModuleName, 1102, "game cannot be parsed")
 )
