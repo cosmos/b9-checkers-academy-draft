3d0bf45487ebb3757069131be979a4da787811e6
Xavier Lepretre <xavier.lepretre@gmail.com>
2022-07-27 16:54:36 +0400

    Added GUI created along createGame message by Ignite.

--- vue/src/store/generated/b9lab/checkers/b9lab.checkers.checkers/module/types/checkers/tx.ts
+++ vue/src/store/generated/b9lab/checkers/b9lab.checkers.checkers/module/types/checkers/tx.ts
@@ -1,13 +1,188 @@
 /* eslint-disable */
+import { Reader, Writer } from "protobufjs/minimal";
+
 export const protobufPackage = "b9lab.checkers.checkers";
 
+export interface MsgCreateGame {
+  creator: string;
+  black: string;
+  red: string;
+}
+
+export interface MsgCreateGameResponse {
+  gameIndex: string;
+}
+
+const baseMsgCreateGame: object = { creator: "", black: "", red: "" };
+
+export const MsgCreateGame = {
+  encode(message: MsgCreateGame, writer: Writer = Writer.create()): Writer {
+    if (message.creator !== "") {
+      writer.uint32(10).string(message.creator);
+    }
+    if (message.black !== "") {
+      writer.uint32(18).string(message.black);
+    }
+    if (message.red !== "") {
+      writer.uint32(26).string(message.red);
+    }
+    return writer;
+  },
+
+  decode(input: Reader | Uint8Array, length?: number): MsgCreateGame {
+    const reader = input instanceof Uint8Array ? new Reader(input) : input;
+    let end = length === undefined ? reader.len : reader.pos + length;
+    const message = { ...baseMsgCreateGame } as MsgCreateGame;
+    while (reader.pos < end) {
+      const tag = reader.uint32();
+      switch (tag >>> 3) {
+        case 1:
+          message.creator = reader.string();
+          break;
+        case 2:
+          message.black = reader.string();
+          break;
+        case 3:
+          message.red = reader.string();
+          break;
+        default:
+          reader.skipType(tag & 7);
+          break;
+      }
+    }
+    return message;
+  },
+
+  fromJSON(object: any): MsgCreateGame {
+    const message = { ...baseMsgCreateGame } as MsgCreateGame;
+    if (object.creator !== undefined && object.creator !== null) {
+      message.creator = String(object.creator);
+    } else {
+      message.creator = "";
+    }
+    if (object.black !== undefined && object.black !== null) {
+      message.black = String(object.black);
+    } else {
+      message.black = "";
+    }
+    if (object.red !== undefined && object.red !== null) {
+      message.red = String(object.red);
+    } else {
+      message.red = "";
+    }
+    return message;
+  },
+
+  toJSON(message: MsgCreateGame): unknown {
+    const obj: any = {};
+    message.creator !== undefined && (obj.creator = message.creator);
+    message.black !== undefined && (obj.black = message.black);
+    message.red !== undefined && (obj.red = message.red);
+    return obj;
+  },
+
+  fromPartial(object: DeepPartial<MsgCreateGame>): MsgCreateGame {
+    const message = { ...baseMsgCreateGame } as MsgCreateGame;
+    if (object.creator !== undefined && object.creator !== null) {
+      message.creator = object.creator;
+    } else {
+      message.creator = "";
+    }
+    if (object.black !== undefined && object.black !== null) {
+      message.black = object.black;
+    } else {
+      message.black = "";
+    }
+    if (object.red !== undefined && object.red !== null) {
+      message.red = object.red;
+    } else {
+      message.red = "";
+    }
+    return message;
+  },
+};
+
+const baseMsgCreateGameResponse: object = { gameIndex: "" };
+
+export const MsgCreateGameResponse = {
+  encode(
+    message: MsgCreateGameResponse,
+    writer: Writer = Writer.create()
+  ): Writer {
+    if (message.gameIndex !== "") {
+      writer.uint32(10).string(message.gameIndex);
+    }
+    return writer;
+  },
+
+  decode(input: Reader | Uint8Array, length?: number): MsgCreateGameResponse {
+    const reader = input instanceof Uint8Array ? new Reader(input) : input;
+    let end = length === undefined ? reader.len : reader.pos + length;
+    const message = { ...baseMsgCreateGameResponse } as MsgCreateGameResponse;
+    while (reader.pos < end) {
+      const tag = reader.uint32();
+      switch (tag >>> 3) {
+        case 1:
+          message.gameIndex = reader.string();
+          break;
+        default:
+          reader.skipType(tag & 7);
+          break;
+      }
+    }
+    return message;
+  },
+
+  fromJSON(object: any): MsgCreateGameResponse {
+    const message = { ...baseMsgCreateGameResponse } as MsgCreateGameResponse;
+    if (object.gameIndex !== undefined && object.gameIndex !== null) {
+      message.gameIndex = String(object.gameIndex);
+    } else {
+      message.gameIndex = "";
+    }
+    return message;
+  },
+
+  toJSON(message: MsgCreateGameResponse): unknown {
+    const obj: any = {};
+    message.gameIndex !== undefined && (obj.gameIndex = message.gameIndex);
+    return obj;
+  },
+
+  fromPartial(
+    object: DeepPartial<MsgCreateGameResponse>
+  ): MsgCreateGameResponse {
+    const message = { ...baseMsgCreateGameResponse } as MsgCreateGameResponse;
+    if (object.gameIndex !== undefined && object.gameIndex !== null) {
+      message.gameIndex = object.gameIndex;
+    } else {
+      message.gameIndex = "";
+    }
+    return message;
+  },
+};
+
 /** Msg defines the Msg service. */
-export interface Msg {}
+export interface Msg {
+  /** this line is used by starport scaffolding # proto/tx/rpc */
+  CreateGame(request: MsgCreateGame): Promise<MsgCreateGameResponse>;
+}
 
 export class MsgClientImpl implements Msg {
   private readonly rpc: Rpc;
   constructor(rpc: Rpc) {
     this.rpc = rpc;
+  }
+  CreateGame(request: MsgCreateGame): Promise<MsgCreateGameResponse> {
+    const data = MsgCreateGame.encode(request).finish();
+    const promise = this.rpc.request(
+      "b9lab.checkers.checkers.Msg",
+      "CreateGame",
+      data
+    );
+    return promise.then((data) =>
+      MsgCreateGameResponse.decode(new Reader(data))
+    );
   }
 }
 
@@ -18,3 +193,14 @@ interface Rpc {
     data: Uint8Array
   ): Promise<Uint8Array>;
 }
+
+type Builtin = Date | Function | Uint8Array | string | number | undefined;
+export type DeepPartial<T> = T extends Builtin
+  ? T
+  : T extends Array<infer U>
+  ? Array<DeepPartial<U>>
+  : T extends ReadonlyArray<infer U>
+  ? ReadonlyArray<DeepPartial<U>>
+  : T extends {}
+  ? { [K in keyof T]?: DeepPartial<T[K]> }
+  : Partial<T>;
