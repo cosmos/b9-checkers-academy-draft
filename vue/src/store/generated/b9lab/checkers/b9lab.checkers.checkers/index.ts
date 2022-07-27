0365f55ab4077d13b8dbc6f4cc2b7cef734bd14c
Xavier Lepretre <xavier.lepretre@gmail.com>
2022-07-27 16:54:36 +0400

    Added GUI created along createGame message by Ignite.

--- vue/src/store/generated/b9lab/checkers/b9lab.checkers.checkers/index.ts
+++ vue/src/store/generated/b9lab/checkers/b9lab.checkers.checkers/index.ts
@@ -230,7 +230,35 @@ export default {
 		},
 		
 		
+		async sendMsgCreateGame({ rootGetters }, { value, fee = [], memo = '' }) {
+			try {
+				const txClient=await initTxClient(rootGetters)
+				const msg = await txClient.msgCreateGame(value)
+				const result = await txClient.signAndBroadcast([msg], {fee: { amount: fee, 
+	gas: "200000" }, memo})
+				return result
+			} catch (e) {
+				if (e == MissingWalletError) {
+					throw new Error('TxClient:MsgCreateGame:Init Could not initialize signing client. Wallet is required.')
+				}else{
+					throw new Error('TxClient:MsgCreateGame:Send Could not broadcast Tx: '+ e.message)
+				}
+			}
+		},
 		
+		async MsgCreateGame({ rootGetters }, { value }) {
+			try {
+				const txClient=await initTxClient(rootGetters)
+				const msg = await txClient.msgCreateGame(value)
+				return msg
+			} catch (e) {
+				if (e == MissingWalletError) {
+					throw new Error('TxClient:MsgCreateGame:Init Could not initialize signing client. Wallet is required.')
+				} else{
+					throw new Error('TxClient:MsgCreateGame:Create Could not create message: ' + e.message)
+				}
+			}
+		},
 		
 	}
 }
