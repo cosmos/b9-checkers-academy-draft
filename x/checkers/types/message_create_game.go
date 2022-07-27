f3ef195bc64c0e68791adfd242daa9d6731593a4
Xavier Lepretre <xavier.lepretre@gmail.com>
2022-07-27 19:59:00 +0400

    Add handling of create game message.
    And unit tests.

--- x/checkers/types/message_create_game.go
+++ x/checkers/types/message_create_game.go
@@ -43,5 +43,13 @@ func (msg *MsgCreateGame) ValidateBasic() error {
 	if err != nil {
 		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
 	}
+	_, err = sdk.AccAddressFromBech32(msg.Black)
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid black address (%s)", err)
+	}
+	_, err = sdk.AccAddressFromBech32(msg.Red)
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid red address (%s)", err)
+	}
 	return nil
 }
