dc8d92f0f6af74e3ecbd988a3228a612f4bb688f
Xavier Lepretre <xavier.lepretre@gmail.com>
2022-07-27 16:54:06 +0400

    Added a message to create a game with Ignite.
    ignite scaffold message createGame black red --module checkers --response gameIndex

---
+++ x/checkers/keeper/msg_server_create_game.go
@@ -0,0 +1,17 @@
+package keeper
+
+import (
+	"context"
+
+	"github.com/b9lab/checkers/x/checkers/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func (k msgServer) CreateGame(goCtx context.Context, msg *types.MsgCreateGame) (*types.MsgCreateGameResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	// TODO: Handling the message
+	_ = ctx
+
+	return &types.MsgCreateGameResponse{}, nil
+}
---
+++ x/checkers/types/message_create_game.go
@@ -0,0 +1,47 @@
+package types
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+)
+
+const TypeMsgCreateGame = "create_game"
+
+var _ sdk.Msg = &MsgCreateGame{}
+
+func NewMsgCreateGame(creator string, black string, red string) *MsgCreateGame {
+	return &MsgCreateGame{
+		Creator: creator,
+		Black:   black,
+		Red:     red,
+	}
+}
+
+func (msg *MsgCreateGame) Route() string {
+	return RouterKey
+}
+
+func (msg *MsgCreateGame) Type() string {
+	return TypeMsgCreateGame
+}
+
+func (msg *MsgCreateGame) GetSigners() []sdk.AccAddress {
+	creator, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		panic(err)
+	}
+	return []sdk.AccAddress{creator}
+}
+
+func (msg *MsgCreateGame) GetSignBytes() []byte {
+	bz := ModuleCdc.MustMarshalJSON(msg)
+	return sdk.MustSortJSON(bz)
+}
+
+func (msg *MsgCreateGame) ValidateBasic() error {
+	_, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
+	}
+	return nil
+}
