e94490601003eac5662c755735404adc5895aca9
Xavier Lepretre <xavier.lepretre@gmail.com>
2022-07-27 17:36:45 +0400

    Add empty create game handler unit test.

---
+++ x/checkers/keeper/msg_server_create_game_test.go
@@ -0,0 +1,27 @@
+package keeper_test
+
+import (
+	"testing"
+
+	"github.com/b9lab/checkers/x/checkers/types"
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	alice = "cosmos1jmjfq0tplp9tmx4v9uemw72y4d2wa5nr3xn9d3"
+	bob   = "cosmos1xyxs3skf3f4jfqeuv89yyaqvjc6lffavxqhc8g"
+	carol = "cosmos1e0w5t53nrq7p66fye6c8p0ynyhf6y24l4yuxd7"
+)
+
+func TestCreateGame(t *testing.T) {
+	msgServer, context := setupMsgServer(t)
+	createResponse, err := msgServer.CreateGame(context, &types.MsgCreateGame{
+		Creator: alice,
+		Black:   bob,
+		Red:     carol,
+	})
+	require.Nil(t, err)
+	require.EqualValues(t, types.MsgCreateGameResponse{
+		GameIndex: "", // TODO: update with a proper value when updated
+	}, *createResponse)
+}
