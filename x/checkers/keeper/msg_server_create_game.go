a5ac98ba7764cfcf8dbfed2457bff3946ac625fc
Xavier Lepretre <xavier.lepretre@gmail.com>
2022-07-27 19:59:00 +0400

    Add handling of create game message.
    And unit tests.

--- x/checkers/keeper/msg_server_create_game.go
+++ x/checkers/keeper/msg_server_create_game.go
@@ -2,7 +2,9 @@ package keeper
 
 import (
 	"context"
+	"strconv"
 
+	"github.com/b9lab/checkers/x/checkers/rules"
 	"github.com/b9lab/checkers/x/checkers/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
@@ -10,8 +12,31 @@ import (
 func (k msgServer) CreateGame(goCtx context.Context, msg *types.MsgCreateGame) (*types.MsgCreateGameResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	systemInfo, found := k.Keeper.GetSystemInfo(ctx)
+	if !found {
+		panic("SystemInfo not found")
+	}
+	newIndex := strconv.FormatUint(systemInfo.NextId, 10)
 
-	return &types.MsgCreateGameResponse{}, nil
+	newGame := rules.New()
+	storedGame := types.StoredGame{
+		Index: newIndex,
+		Board: newGame.String(),
+		Turn:  rules.PieceStrings[newGame.Turn],
+		Black: msg.Black,
+		Red:   msg.Red,
+	}
+
+	err := storedGame.Validate()
+	if err != nil {
+		return nil, err
+	}
+
+	k.Keeper.SetStoredGame(ctx, storedGame)
+	systemInfo.NextId++
+	k.Keeper.SetSystemInfo(ctx, systemInfo)
+
+	return &types.MsgCreateGameResponse{
+		GameIndex: newIndex,
+	}, nil
 }
