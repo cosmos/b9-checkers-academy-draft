e2a87fa30a208d37dbe7e9203296ac82e0872278
Xavier Lepretre <xavier.lepretre@gmail.com>
2022-07-28 17:43:00 +0400

    Add events to message handling.

--- x/checkers/keeper/msg_server_play_move_test.go
+++ x/checkers/keeper/msg_server_play_move_test.go
@@ -106,6 +106,33 @@ func TestPlayMoveSavedGame(t *testing.T) {
 	}, game1)
 }
 
+func TestPlayMoveEmitted(t *testing.T) {
+	msgServer, _, context := setupMsgServerWithOneGameForPlayMove(t)
+	msgServer.PlayMove(context, &types.MsgPlayMove{
+		Creator:   bob,
+		GameIndex: "1",
+		FromX:     1,
+		FromY:     2,
+		ToX:       2,
+		ToY:       3,
+	})
+	ctx := sdk.UnwrapSDKContext(context)
+	require.NotNil(t, ctx)
+	events := sdk.StringifyEvents(ctx.EventManager().ABCIEvents())
+	require.Len(t, events, 2)
+	event := events[0]
+	require.EqualValues(t, sdk.StringEvent{
+		Type: "move-played",
+		Attributes: []sdk.Attribute{
+			{Key: "creator", Value: bob},
+			{Key: "game-index", Value: "1"},
+			{Key: "captured-x", Value: "-1"},
+			{Key: "captured-y", Value: "-1"},
+			{Key: "winner", Value: "*"},
+		},
+	}, event)
+}
+
 func TestPlayMoveNotPlayer(t *testing.T) {
 	msgServer, _, context := setupMsgServerWithOneGameForPlayMove(t)
 	playMoveResponse, err := msgServer.PlayMove(context, &types.MsgPlayMove{
@@ -227,6 +254,39 @@ func TestPlayMove2SavedGame(t *testing.T) {
 		Black: bob,
 		Red:   carol,
 	}, game1)
+}
+
+func TestPlayMove2Emitted(t *testing.T) {
+	msgServer, _, context := setupMsgServerWithOneGameForPlayMove(t)
+	msgServer.PlayMove(context, &types.MsgPlayMove{
+		Creator:   bob,
+		GameIndex: "1",
+		FromX:     1,
+		FromY:     2,
+		ToX:       2,
+		ToY:       3,
+	})
+	msgServer.PlayMove(context, &types.MsgPlayMove{
+		Creator:   carol,
+		GameIndex: "1",
+		FromX:     0,
+		FromY:     5,
+		ToX:       1,
+		ToY:       4,
+	})
+	ctx := sdk.UnwrapSDKContext(context)
+	require.NotNil(t, ctx)
+	events := sdk.StringifyEvents(ctx.EventManager().ABCIEvents())
+	require.Len(t, events, 2)
+	event := events[0]
+	require.Equal(t, "move-played", event.Type)
+	require.EqualValues(t, []sdk.Attribute{
+		{Key: "creator", Value: carol},
+		{Key: "game-index", Value: "1"},
+		{Key: "captured-x", Value: "-1"},
+		{Key: "captured-y", Value: "-1"},
+		{Key: "winner", Value: "*"},
+	}, event.Attributes[5:])
 }
 
 func TestPlayMove3(t *testing.T) {
