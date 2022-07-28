da5c7f8a781e6bc295ef9c493799e95a18aa6b4d
Xavier Lepretre <xavier.lepretre@gmail.com>
2022-07-28 11:43:40 +0400

    Added a message to play a game with Ignite.
    ignite scaffold message playMove gameIndex fromX:uint fromY:uint toX:uint toY:uint --module checkers --response capturedX:int,capturedY:int,winner

--- vue/src/store/generated/b9lab/checkers/b9lab.checkers.checkers/index.ts
+++ vue/src/store/generated/b9lab/checkers/b9lab.checkers.checkers/index.ts
@@ -245,6 +245,21 @@ export default {
 				}
 			}
 		},
+		async sendMsgPlayMove({ rootGetters }, { value, fee = [], memo = '' }) {
+			try {
+				const txClient=await initTxClient(rootGetters)
+				const msg = await txClient.msgPlayMove(value)
+				const result = await txClient.signAndBroadcast([msg], {fee: { amount: fee, 
+	gas: "200000" }, memo})
+				return result
+			} catch (e) {
+				if (e == MissingWalletError) {
+					throw new Error('TxClient:MsgPlayMove:Init Could not initialize signing client. Wallet is required.')
+				}else{
+					throw new Error('TxClient:MsgPlayMove:Send Could not broadcast Tx: '+ e.message)
+				}
+			}
+		},
 		
 		async MsgCreateGame({ rootGetters }, { value }) {
 			try {
@@ -256,6 +271,19 @@ export default {
 					throw new Error('TxClient:MsgCreateGame:Init Could not initialize signing client. Wallet is required.')
 				} else{
 					throw new Error('TxClient:MsgCreateGame:Create Could not create message: ' + e.message)
+				}
+			}
+		},
+		async MsgPlayMove({ rootGetters }, { value }) {
+			try {
+				const txClient=await initTxClient(rootGetters)
+				const msg = await txClient.msgPlayMove(value)
+				return msg
+			} catch (e) {
+				if (e == MissingWalletError) {
+					throw new Error('TxClient:MsgPlayMove:Init Could not initialize signing client. Wallet is required.')
+				} else{
+					throw new Error('TxClient:MsgPlayMove:Create Could not create message: ' + e.message)
 				}
 			}
 		},
--- vue/src/store/generated/b9lab/checkers/b9lab.checkers.checkers/module/types/checkers/tx.ts
+++ vue/src/store/generated/b9lab/checkers/b9lab.checkers.checkers/module/types/checkers/tx.ts
@@ -1,5 +1,6 @@
 /* eslint-disable */
-import { Reader, Writer } from "protobufjs/minimal";
+import { Reader, util, configure, Writer } from "protobufjs/minimal";
+import * as Long from "long";
 
 export const protobufPackage = "b9lab.checkers.checkers";
 
@@ -11,6 +12,21 @@ export interface MsgCreateGame {
 
 export interface MsgCreateGameResponse {
   gameIndex: string;
+}
+
+export interface MsgPlayMove {
+  creator: string;
+  gameIndex: string;
+  fromX: number;
+  fromY: number;
+  toX: number;
+  toY: number;
+}
+
+export interface MsgPlayMoveResponse {
+  capturedX: number;
+  capturedY: number;
+  winner: string;
 }
 
 const baseMsgCreateGame: object = { creator: "", black: "", red: "" };
@@ -162,10 +178,254 @@ export const MsgCreateGameResponse = {
   },
 };
 
+const baseMsgPlayMove: object = {
+  creator: "",
+  gameIndex: "",
+  fromX: 0,
+  fromY: 0,
+  toX: 0,
+  toY: 0,
+};
+
+export const MsgPlayMove = {
+  encode(message: MsgPlayMove, writer: Writer = Writer.create()): Writer {
+    if (message.creator !== "") {
+      writer.uint32(10).string(message.creator);
+    }
+    if (message.gameIndex !== "") {
+      writer.uint32(18).string(message.gameIndex);
+    }
+    if (message.fromX !== 0) {
+      writer.uint32(24).uint64(message.fromX);
+    }
+    if (message.fromY !== 0) {
+      writer.uint32(32).uint64(message.fromY);
+    }
+    if (message.toX !== 0) {
+      writer.uint32(40).uint64(message.toX);
+    }
+    if (message.toY !== 0) {
+      writer.uint32(48).uint64(message.toY);
+    }
+    return writer;
+  },
+
+  decode(input: Reader | Uint8Array, length?: number): MsgPlayMove {
+    const reader = input instanceof Uint8Array ? new Reader(input) : input;
+    let end = length === undefined ? reader.len : reader.pos + length;
+    const message = { ...baseMsgPlayMove } as MsgPlayMove;
+    while (reader.pos < end) {
+      const tag = reader.uint32();
+      switch (tag >>> 3) {
+        case 1:
+          message.creator = reader.string();
+          break;
+        case 2:
+          message.gameIndex = reader.string();
+          break;
+        case 3:
+          message.fromX = longToNumber(reader.uint64() as Long);
+          break;
+        case 4:
+          message.fromY = longToNumber(reader.uint64() as Long);
+          break;
+        case 5:
+          message.toX = longToNumber(reader.uint64() as Long);
+          break;
+        case 6:
+          message.toY = longToNumber(reader.uint64() as Long);
+          break;
+        default:
+          reader.skipType(tag & 7);
+          break;
+      }
+    }
+    return message;
+  },
+
+  fromJSON(object: any): MsgPlayMove {
+    const message = { ...baseMsgPlayMove } as MsgPlayMove;
+    if (object.creator !== undefined && object.creator !== null) {
+      message.creator = String(object.creator);
+    } else {
+      message.creator = "";
+    }
+    if (object.gameIndex !== undefined && object.gameIndex !== null) {
+      message.gameIndex = String(object.gameIndex);
+    } else {
+      message.gameIndex = "";
+    }
+    if (object.fromX !== undefined && object.fromX !== null) {
+      message.fromX = Number(object.fromX);
+    } else {
+      message.fromX = 0;
+    }
+    if (object.fromY !== undefined && object.fromY !== null) {
+      message.fromY = Number(object.fromY);
+    } else {
+      message.fromY = 0;
+    }
+    if (object.toX !== undefined && object.toX !== null) {
+      message.toX = Number(object.toX);
+    } else {
+      message.toX = 0;
+    }
+    if (object.toY !== undefined && object.toY !== null) {
+      message.toY = Number(object.toY);
+    } else {
+      message.toY = 0;
+    }
+    return message;
+  },
+
+  toJSON(message: MsgPlayMove): unknown {
+    const obj: any = {};
+    message.creator !== undefined && (obj.creator = message.creator);
+    message.gameIndex !== undefined && (obj.gameIndex = message.gameIndex);
+    message.fromX !== undefined && (obj.fromX = message.fromX);
+    message.fromY !== undefined && (obj.fromY = message.fromY);
+    message.toX !== undefined && (obj.toX = message.toX);
+    message.toY !== undefined && (obj.toY = message.toY);
+    return obj;
+  },
+
+  fromPartial(object: DeepPartial<MsgPlayMove>): MsgPlayMove {
+    const message = { ...baseMsgPlayMove } as MsgPlayMove;
+    if (object.creator !== undefined && object.creator !== null) {
+      message.creator = object.creator;
+    } else {
+      message.creator = "";
+    }
+    if (object.gameIndex !== undefined && object.gameIndex !== null) {
+      message.gameIndex = object.gameIndex;
+    } else {
+      message.gameIndex = "";
+    }
+    if (object.fromX !== undefined && object.fromX !== null) {
+      message.fromX = object.fromX;
+    } else {
+      message.fromX = 0;
+    }
+    if (object.fromY !== undefined && object.fromY !== null) {
+      message.fromY = object.fromY;
+    } else {
+      message.fromY = 0;
+    }
+    if (object.toX !== undefined && object.toX !== null) {
+      message.toX = object.toX;
+    } else {
+      message.toX = 0;
+    }
+    if (object.toY !== undefined && object.toY !== null) {
+      message.toY = object.toY;
+    } else {
+      message.toY = 0;
+    }
+    return message;
+  },
+};
+
+const baseMsgPlayMoveResponse: object = {
+  capturedX: 0,
+  capturedY: 0,
+  winner: "",
+};
+
+export const MsgPlayMoveResponse = {
+  encode(
+    message: MsgPlayMoveResponse,
+    writer: Writer = Writer.create()
+  ): Writer {
+    if (message.capturedX !== 0) {
+      writer.uint32(8).int32(message.capturedX);
+    }
+    if (message.capturedY !== 0) {
+      writer.uint32(16).int32(message.capturedY);
+    }
+    if (message.winner !== "") {
+      writer.uint32(26).string(message.winner);
+    }
+    return writer;
+  },
+
+  decode(input: Reader | Uint8Array, length?: number): MsgPlayMoveResponse {
+    const reader = input instanceof Uint8Array ? new Reader(input) : input;
+    let end = length === undefined ? reader.len : reader.pos + length;
+    const message = { ...baseMsgPlayMoveResponse } as MsgPlayMoveResponse;
+    while (reader.pos < end) {
+      const tag = reader.uint32();
+      switch (tag >>> 3) {
+        case 1:
+          message.capturedX = reader.int32();
+          break;
+        case 2:
+          message.capturedY = reader.int32();
+          break;
+        case 3:
+          message.winner = reader.string();
+          break;
+        default:
+          reader.skipType(tag & 7);
+          break;
+      }
+    }
+    return message;
+  },
+
+  fromJSON(object: any): MsgPlayMoveResponse {
+    const message = { ...baseMsgPlayMoveResponse } as MsgPlayMoveResponse;
+    if (object.capturedX !== undefined && object.capturedX !== null) {
+      message.capturedX = Number(object.capturedX);
+    } else {
+      message.capturedX = 0;
+    }
+    if (object.capturedY !== undefined && object.capturedY !== null) {
+      message.capturedY = Number(object.capturedY);
+    } else {
+      message.capturedY = 0;
+    }
+    if (object.winner !== undefined && object.winner !== null) {
+      message.winner = String(object.winner);
+    } else {
+      message.winner = "";
+    }
+    return message;
+  },
+
+  toJSON(message: MsgPlayMoveResponse): unknown {
+    const obj: any = {};
+    message.capturedX !== undefined && (obj.capturedX = message.capturedX);
+    message.capturedY !== undefined && (obj.capturedY = message.capturedY);
+    message.winner !== undefined && (obj.winner = message.winner);
+    return obj;
+  },
+
+  fromPartial(object: DeepPartial<MsgPlayMoveResponse>): MsgPlayMoveResponse {
+    const message = { ...baseMsgPlayMoveResponse } as MsgPlayMoveResponse;
+    if (object.capturedX !== undefined && object.capturedX !== null) {
+      message.capturedX = object.capturedX;
+    } else {
+      message.capturedX = 0;
+    }
+    if (object.capturedY !== undefined && object.capturedY !== null) {
+      message.capturedY = object.capturedY;
+    } else {
+      message.capturedY = 0;
+    }
+    if (object.winner !== undefined && object.winner !== null) {
+      message.winner = object.winner;
+    } else {
+      message.winner = "";
+    }
+    return message;
+  },
+};
+
 /** Msg defines the Msg service. */
 export interface Msg {
-  /** this line is used by starport scaffolding # proto/tx/rpc */
   CreateGame(request: MsgCreateGame): Promise<MsgCreateGameResponse>;
+  /** this line is used by starport scaffolding # proto/tx/rpc */
+  PlayMove(request: MsgPlayMove): Promise<MsgPlayMoveResponse>;
 }
 
 export class MsgClientImpl implements Msg {
@@ -184,6 +444,16 @@ export class MsgClientImpl implements Msg {
       MsgCreateGameResponse.decode(new Reader(data))
     );
   }
+
+  PlayMove(request: MsgPlayMove): Promise<MsgPlayMoveResponse> {
+    const data = MsgPlayMove.encode(request).finish();
+    const promise = this.rpc.request(
+      "b9lab.checkers.checkers.Msg",
+      "PlayMove",
+      data
+    );
+    return promise.then((data) => MsgPlayMoveResponse.decode(new Reader(data)));
+  }
 }
 
 interface Rpc {
@@ -193,6 +463,16 @@ interface Rpc {
     data: Uint8Array
   ): Promise<Uint8Array>;
 }
+
+declare var self: any | undefined;
+declare var window: any | undefined;
+var globalThis: any = (() => {
+  if (typeof globalThis !== "undefined") return globalThis;
+  if (typeof self !== "undefined") return self;
+  if (typeof window !== "undefined") return window;
+  if (typeof global !== "undefined") return global;
+  throw "Unable to locate global object";
+})();
 
 type Builtin = Date | Function | Uint8Array | string | number | undefined;
 export type DeepPartial<T> = T extends Builtin
@@ -204,3 +484,15 @@ export type DeepPartial<T> = T extends Builtin
   : T extends {}
   ? { [K in keyof T]?: DeepPartial<T[K]> }
   : Partial<T>;
+
+function longToNumber(long: Long): number {
+  if (long.gt(Number.MAX_SAFE_INTEGER)) {
+    throw new globalThis.Error("Value is larger than Number.MAX_SAFE_INTEGER");
+  }
+  return long.toNumber();
+}
+
+if (util.Long !== Long) {
+  util.Long = Long as any;
+  configure();
+}
