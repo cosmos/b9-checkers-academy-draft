c51550001e62d48c3022e28c2837e611c5bb0be8
Xavier Lepretre <xavier.lepretre@gmail.com>
2022-07-28 11:43:40 +0400

    Added a message to play a game with Ignite.
    ignite scaffold message playMove gameIndex fromX:uint fromY:uint toX:uint toY:uint --module checkers --response capturedX:int,capturedY:int,winner

--- x/checkers/types/codec.go
+++ x/checkers/types/codec.go
@@ -9,12 +9,16 @@ import (
 
 func RegisterCodec(cdc *codec.LegacyAmino) {
 	cdc.RegisterConcrete(&MsgCreateGame{}, "checkers/CreateGame", nil)
+	cdc.RegisterConcrete(&MsgPlayMove{}, "checkers/PlayMove", nil)
 	// this line is used by starport scaffolding # 2
 }
 
 func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
 	registry.RegisterImplementations((*sdk.Msg)(nil),
 		&MsgCreateGame{},
+	)
+	registry.RegisterImplementations((*sdk.Msg)(nil),
+		&MsgPlayMove{},
 	)
 	// this line is used by starport scaffolding # 3
 
