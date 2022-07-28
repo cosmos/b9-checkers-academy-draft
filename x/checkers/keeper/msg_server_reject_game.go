d090ff48260a0e4c20d52d2c1ac28cedfba27767
Xavier Lepretre <xavier.lepretre@gmail.com>
2022-07-28 21:16:45 +0400

    Add handling of reject game message.

--- x/checkers/keeper/msg_server_reject_game.go
+++ x/checkers/keeper/msg_server_reject_game.go
@@ -5,13 +5,37 @@ import (
 
 	"github.com/b9lab/checkers/x/checkers/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 func (k msgServer) RejectGame(goCtx context.Context, msg *types.MsgRejectGame) (*types.MsgRejectGameResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	storedGame, found := k.Keeper.GetStoredGame(ctx, msg.GameIndex)
+	if !found {
+		return nil, sdkerrors.Wrapf(types.ErrGameNotFound, "%s", msg.GameIndex)
+	}
+
+	if storedGame.Black == msg.Creator {
+		if 0 < storedGame.MoveCount { // Notice the use of the new field
+			return nil, types.ErrBlackAlreadyPlayed
+		}
+	} else if storedGame.Red == msg.Creator {
+		if 1 < storedGame.MoveCount { // Notice the use of the new field
+			return nil, types.ErrRedAlreadyPlayed
+		}
+	} else {
+		return nil, sdkerrors.Wrapf(types.ErrCreatorNotPlayer, "%s", msg.Creator)
+	}
+
+	k.Keeper.RemoveStoredGame(ctx, msg.GameIndex)
+
+	ctx.EventManager().EmitEvent(
+		sdk.NewEvent(types.GameRejectedEventType,
+			sdk.NewAttribute(types.GameRejectedEventCreator, msg.Creator),
+			sdk.NewAttribute(types.GameRejectedEventGameIndex, msg.GameIndex),
+		),
+	)
 
 	return &types.MsgRejectGameResponse{}, nil
 }
