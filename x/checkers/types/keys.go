1ed41c229eacee444e7031c053669fb29ff3f47d
Xavier Lepretre <xavier.lepretre@gmail.com>
2022-07-29 23:58:00 +0400

    Add game forfeit and tests.

--- x/checkers/types/keys.go
+++ x/checkers/types/keys.go
@@ -59,3 +59,10 @@ const (
 	MaxTurnDuration = time.Duration(24 * 3_600 * 1000_000_000) // 1 day
 	DeadlineLayout  = "2006-01-02 15:04:05.999999999 +0000 UTC"
 )
+
+const (
+	GameForfeitedEventType      = "game-forfeited"
+	GameForfeitedEventGameIndex = "game-index"
+	GameForfeitedEventWinner    = "winner"
+	GameForfeitedEventBoard     = "board"
+)
