cf3e896a02765287ed278d8df38d4b51df99f08d
Xavier Lepretre <xavier.lepretre@gmail.com>
2022-07-29 19:17:34 +0400

    Add game winner field and fixes.

--- x/checkers/keeper/msg_server_create_game_test.go
+++ x/checkers/keeper/msg_server_create_game_test.go
@@ -47,11 +47,12 @@ func TestCreate1GameHasSaved(t *testing.T) {
 	game1, found1 := keeper.GetStoredGame(ctx, "1")
 	require.True(t, found1)
 	require.EqualValues(t, types.StoredGame{
-		Index: "1",
-		Board: "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
-		Turn:  "b",
-		Black: bob,
-		Red:   carol,
+		Index:  "1",
+		Board:  "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:   "b",
+		Black:  bob,
+		Red:    carol,
+		Winner: "*",
 	}, game1)
 }
 
@@ -65,11 +66,12 @@ func TestCreate1GameGetAll(t *testing.T) {
 	games := keeper.GetAllStoredGame(sdk.UnwrapSDKContext(context))
 	require.Len(t, games, 1)
 	require.EqualValues(t, types.StoredGame{
-		Index: "1",
-		Board: "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
-		Turn:  "b",
-		Black: bob,
-		Red:   carol,
+		Index:  "1",
+		Board:  "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:   "b",
+		Black:  bob,
+		Red:    carol,
+		Winner: "*",
 	}, games[0])
 }
 
@@ -175,29 +177,32 @@ func TestCreate3GamesHasSaved(t *testing.T) {
 	game1, found1 := keeper.GetStoredGame(ctx, "1")
 	require.True(t, found1)
 	require.EqualValues(t, types.StoredGame{
-		Index: "1",
-		Board: "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
-		Turn:  "b",
-		Black: bob,
-		Red:   carol,
+		Index:  "1",
+		Board:  "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:   "b",
+		Black:  bob,
+		Red:    carol,
+		Winner: "*",
 	}, game1)
 	game2, found2 := keeper.GetStoredGame(ctx, "2")
 	require.True(t, found2)
 	require.EqualValues(t, types.StoredGame{
-		Index: "2",
-		Board: "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
-		Turn:  "b",
-		Black: carol,
-		Red:   alice,
+		Index:  "2",
+		Board:  "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:   "b",
+		Black:  carol,
+		Red:    alice,
+		Winner: "*",
 	}, game2)
 	game3, found3 := keeper.GetStoredGame(ctx, "3")
 	require.True(t, found3)
 	require.EqualValues(t, types.StoredGame{
-		Index: "3",
-		Board: "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
-		Turn:  "b",
-		Black: alice,
-		Red:   bob,
+		Index:  "3",
+		Board:  "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:   "b",
+		Black:  alice,
+		Red:    bob,
+		Winner: "*",
 	}, game3)
 }
 
@@ -221,25 +226,28 @@ func TestCreate3GamesGetAll(t *testing.T) {
 	games := keeper.GetAllStoredGame(sdk.UnwrapSDKContext(context))
 	require.Len(t, games, 3)
 	require.EqualValues(t, types.StoredGame{
-		Index: "1",
-		Board: "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
-		Turn:  "b",
-		Black: bob,
-		Red:   carol,
+		Index:  "1",
+		Board:  "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:   "b",
+		Black:  bob,
+		Red:    carol,
+		Winner: "*",
 	}, games[0])
 	require.EqualValues(t, types.StoredGame{
-		Index: "2",
-		Board: "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
-		Turn:  "b",
-		Black: carol,
-		Red:   alice,
+		Index:  "2",
+		Board:  "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:   "b",
+		Black:  carol,
+		Red:    alice,
+		Winner: "*",
 	}, games[1])
 	require.EqualValues(t, types.StoredGame{
-		Index: "3",
-		Board: "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
-		Turn:  "b",
-		Black: alice,
-		Red:   bob,
+		Index:  "3",
+		Board:  "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:   "b",
+		Black:  alice,
+		Red:    bob,
+		Winner: "*",
 	}, games[2])
 }
 
@@ -267,10 +275,11 @@ func TestCreateGameFarFuture(t *testing.T) {
 	game1, found1 := keeper.GetStoredGame(ctx, "1024")
 	require.True(t, found1)
 	require.EqualValues(t, types.StoredGame{
-		Index: "1024",
-		Board: "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
-		Turn:  "b",
-		Black: bob,
-		Red:   carol,
+		Index:  "1024",
+		Board:  "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:   "b",
+		Black:  bob,
+		Red:    carol,
+		Winner: "*",
 	}, game1)
 }
---
+++ x/checkers/keeper/msg_server_play_move_winner_test.go
@@ -0,0 +1,46 @@
+package keeper_test
+
+import (
+	"testing"
+
+	"github.com/b9lab/checkers/x/checkers/testutil"
+	"github.com/b9lab/checkers/x/checkers/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPlayMoveUpToWinner(t *testing.T) {
+	msgServer, keeper, context := setupMsgServerWithOneGameForPlayMove(t)
+	ctx := sdk.UnwrapSDKContext(context)
+
+	testutil.PlayAllMoves(t, msgServer, context, "1", bob, carol, testutil.Game1Moves)
+
+	systemInfo, found := keeper.GetSystemInfo(ctx)
+	require.True(t, found)
+	require.EqualValues(t, types.SystemInfo{
+		NextId: 2,
+	}, systemInfo)
+
+	game, found := keeper.GetStoredGame(ctx, "1")
+	require.True(t, found)
+	require.EqualValues(t, types.StoredGame{
+		Index:  "1",
+		Board:  "",
+		Turn:   "b",
+		Black:  bob,
+		Red:    carol,
+		Winner: "b",
+	}, game)
+	events := sdk.StringifyEvents(ctx.EventManager().ABCIEvents())
+	require.Len(t, events, 2)
+	event := events[0]
+	require.Equal(t, event.Type, "move-played")
+	require.EqualValues(t, []sdk.Attribute{
+		{Key: "creator", Value: bob},
+		{Key: "game-index", Value: "1"},
+		{Key: "captured-x", Value: "2"},
+		{Key: "captured-y", Value: "5"},
+		{Key: "winner", Value: "b"},
+		{Key: "board", Value: "*b*b****|**b*b***|*****b**|********|***B****|********|*****b**|********"},
+	}, event.Attributes[(len(testutil.Game1Moves)-1)*6:])
+}
