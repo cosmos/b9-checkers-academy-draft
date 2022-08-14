99de1324a08c2837377457603e876305dd71be6f
Xavier Lepretre <xavier.lepretre@gmail.com>
2022-08-14 16:07:10 +0400

    Add can play query handling and tests.

---
+++ tests/integration/checkers/keeper/grpc_query_can_play_move_test.go
@@ -0,0 +1,26 @@
+package keeper_test
+
+import (
+	"github.com/b9lab/checkers/x/checkers/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+var canPlayOkResponse = &types.QueryCanPlayMoveResponse{
+	Possible: true,
+	Reason:   "ok",
+}
+
+func (suite *IntegrationTestSuite) TestCanPlayAfterCreate() {
+	suite.setupSuiteWithOneGameForPlayMove()
+	goCtx := sdk.WrapSDKContext(suite.ctx)
+	response, err := suite.queryClient.CanPlayMove(goCtx, &types.QueryCanPlayMoveRequest{
+		GameIndex: "1",
+		Player:    "b",
+		FromX:     1,
+		FromY:     2,
+		ToX:       2,
+		ToY:       3,
+	})
+	suite.Require().Nil(err)
+	suite.Require().EqualValues(canPlayOkResponse, response)
+}
