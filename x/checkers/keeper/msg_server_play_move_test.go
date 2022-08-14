c670dcc3efb8f08fcaa1e18f7c94aa9d1d03a23e
Xavier Lepretre <xavier.lepretre@gmail.com>
2022-08-14 13:35:01 +0400

    Add gas handling and unit tests.

--- x/checkers/keeper/msg_server_play_move_test.go
+++ x/checkers/keeper/msg_server_play_move_test.go
@@ -173,6 +173,24 @@ func TestPlayMoveCalledBank(t *testing.T) {
 	})
 }
 
+func TestPlayMoveConsumedGas(t *testing.T) {
+	msgServer, _, context, ctrl, escrow := setupMsgServerWithOneGameForPlayMove(t)
+	ctx := sdk.UnwrapSDKContext(context)
+	defer ctrl.Finish()
+	escrow.ExpectAny(context)
+	before := ctx.GasMeter().GasConsumed()
+	msgServer.PlayMove(context, &types.MsgPlayMove{
+		Creator:   bob,
+		GameIndex: "1",
+		FromX:     1,
+		FromY:     2,
+		ToX:       2,
+		ToY:       3,
+	})
+	after := ctx.GasMeter().GasConsumed()
+	require.GreaterOrEqual(t, after, before+5_000)
+}
+
 func TestPlayMoveNotPlayer(t *testing.T) {
 	msgServer, _, context, ctrl, _ := setupMsgServerWithOneGameForPlayMove(t)
 	defer ctrl.Finish()
