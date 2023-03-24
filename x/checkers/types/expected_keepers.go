c953493b1d4df2952732d3fb805e1f2ff0f4d542
Xavier Leprêtre <xavier.lepretre@gmail.com>
2023-03-25 00:40:41 +0400

    Add wager handling and tests.

--- x/checkers/types/expected_keepers.go
+++ x/checkers/types/expected_keepers.go
@@ -16,3 +16,8 @@ type BankKeeper interface {
 	SpendableCoins(ctx sdk.Context, addr sdk.AccAddress) sdk.Coins
 	// Methods imported from bank should be defined here
 }
+
+type BankEscrowKeeper interface {
+	SendCoinsFromModuleToAccount(ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
+	SendCoinsFromAccountToModule(ctx sdk.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
+}
