5a50aa0a506215e4e7d43c58ed101c6e06fff5dd
Francesco De Falco <38818486+francesco-defalco@users.noreply.github.com>
2023-05-10 19:01:36 +0200

    Update msg_server_test.go
    A simple test on MsgServer

--- x/checkers/keeper/msg_server_test.go
+++ x/checkers/keeper/msg_server_test.go
@@ -4,13 +4,20 @@ import (
 	"context"
 	"testing"
 
-	keepertest "github.com/b9lab/checkers/testutil/keeper"
-	"github.com/b9lab/checkers/x/checkers/keeper"
-	"github.com/b9lab/checkers/x/checkers/types"
+	keepertest "github.com/b9blab/checkers/testutil/keeper"
+	"github.com/b9blab/checkers/x/checkers/keeper"
+	"github.com/b9blab/checkers/x/checkers/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
 )
 
 func setupMsgServer(t testing.TB) (types.MsgServer, context.Context) {
 	k, ctx := keepertest.CheckersKeeper(t)
 	return keeper.NewMsgServerImpl(*k), sdk.WrapSDKContext(ctx)
+}
+
+func TestMsgServer(t *testing.T) {
+	msgServer, context := setupMsgServer(t)
+	require.NotNil(t, msgServer)
+	require.NotNil(t, context)
 }
