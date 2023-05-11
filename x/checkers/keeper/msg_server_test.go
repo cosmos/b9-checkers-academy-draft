39ed9edc112de72084caa03d61c38a7ff8fbee04
Francesco De Falco <38818486+francesco-defalco@users.noreply.github.com>
2023-05-11 21:48:32 +0200

    Update msg_server_test.go

--- x/checkers/keeper/msg_server_test.go
+++ x/checkers/keeper/msg_server_test.go
@@ -4,9 +4,9 @@ import (
 	"context"
 	"testing"
 
-	keepertest "github.com/b9blab/checkers/testutil/keeper"
-	"github.com/b9blab/checkers/x/checkers/keeper"
-	"github.com/b9blab/checkers/x/checkers/types"
+	keepertest "github.com/b9lab/checkers/testutil/keeper"
+	"github.com/b9lab/checkers/x/checkers/keeper"
+	"github.com/b9lab/checkers/x/checkers/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/require"
 )
